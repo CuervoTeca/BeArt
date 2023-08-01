d0409d3754d707f2a73eaac502ce99f73ee3877f
Guillermo.SDX <memo.leved@gmail.com>
2023-08-01 12:54:52 -0700

    Ahora solo el front

---
+++ front-end/src/components/Modal Registro/ModalSignUp.tsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCol, IonContent, 
+  IonHeader, IonIcon, IonInput, IonItem, IonLabel,
+   IonModal,IonText,IonTitle, IonToast, IonToolbar } from '@ionic/react';
+import { close } from 'ionicons/icons';
+import './ModalSignUp.css';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+
+const ModalSignUp: React.FC = () => {
+
+  //Abrir el modal
+  const [showModal, setShowModal] = useState(false);
+  // Data
+  const [formData, setFormData] = useState({
+    name: '',
+    lastname: '',
+    email: '',
+    birthday:'',
+    password: '',
+  });
+  // abrir el modal
+  const openModal = () => {
+    setShowModal(true);
+  }
+  // cerrar el modal
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
+  // Función para manejar los cambios en los campos de entrada del formulario
+  const handleInputChange = (event: any) => {
+  // Extraer el nombre y el valor del campo de entrada del evento
+  const { name, value } = event.target;
+  // Actualizar el estado del formulario (formData) con los nuevos valores
+  setFormData({ ...formData, [name]: value, });
+};
+
+const [ShowSuccessToast, setShowSuccessToast] = useState(false);
+const [ShowErrorToast, setShowErrorToast] = useState(false);
+ 
+  //Publica el registro en la api
+  const handleRegister = async (event: any) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/register', formData);
+      console.log(response.data); // La respuesta del servidor, por ejemplo, mensaje de éxito o error
+     // Cerrar Modal
+     setTimeout(() => {
+      closeModal();
+    }, 3000);
+
+      // Mostrar el mensaje de éxito
+      setShowSuccessToast(true);
+      console.log('SUUUUUUUUPEEEER!');
+    } catch (error) {
+      console.error(error)
+      setShowErrorToast(true);
+      // Manejar el error en caso de que ocurra
+
+    }
+  };
+  
+  return (
+    <>
+    <IonButton expand="full" color="secondary" onClick={openModal}>
+        Crear una cuenta
+    </IonButton>
+    <IonModal isOpen={showModal} onDidDismiss={closeModal}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle size='large'>
+            <div className="Close">
+            Crear una cuenta
+              <IonButtons slot="end" onClick={closeModal}>
+                <IonIcon slot="icon-only" icon={close} />
+              </IonButtons>
+            </div>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={handleRegister}>
+              <IonItem>
+                <IonLabel position="floating">Nombre</IonLabel>
+                <IonInput
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onIonChange={handleInputChange}
+                  required
+                />
+              </IonItem>
+              <IonItem>
+              <IonLabel position="floating">Apellido</IonLabel>
+              <IonInput
+                type="text"
+                name="lastname" // Nombre del campo de apellido
+                value={formData.lastname}
+                onIonChange={handleInputChange}
+                required
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">Correo electrónico</IonLabel>
+                <IonInput
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onIonChange={handleInputChange}
+                  required
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position='stacked'>Fecha de nacimiento</IonLabel>
+                <IonInput
+                  type="date"
+                  name="birthday"
+                  value={formData.birthday}
+                  onIonChange={handleInputChange}
+                  required
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">Contraseña</IonLabel>
+                <IonInput
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onIonChange={handleInputChange}
+                  required
+                  minlength={6}
+                />
+                {formData.password.length < 8 && (
+                  <IonText color="danger" >La contraseña debe tener al menos 8 caracteres.</IonText>
+              )}
+
+              </IonItem>
+              <IonButton className="button-margin" expand="full" type="submit">
+                Registrarse
+              </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
+        <IonToast
+          isOpen={true} // El Toast se mostrará automáticamente cuando se renderice
+          onDidDismiss={() => {}}
+          message="Registro exitoso!"
+          duration={3000}
+        />
+      </IonContent>
+      <IonToast
+          isOpen={ShowSuccessToast}
+          onDidDismiss={() => setShowSuccessToast(false)}
+          message="Registro exitoso!"
+          duration={3000}
+        />
+        <IonToast
+          isOpen={ShowErrorToast}
+          onDidDismiss={() => setShowErrorToast(false)}
+          message="Nombre o correo en uso."
+          duration={5000}
+        />
+    </IonModal>
+  </>
+  );
+}
+
+export default ModalSignUp;
---
+++ front-end/src/pages/Login/Login.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonList, IonLabel, IonInput, IonButton, IonCol, IonRow, IonGrid, IonLoading, IonToast } from '@ionic/react';
+import axios from 'axios';
+import './Login.css';
+import ModalSignUp from '../../components/Modal Registro/ModalSignUp';
+import { useHistory } from 'react-router-dom';
+import ToastsComponent from '../../components/ToastsComponentProps/ToastComponent';
+import ModalContraseña from '../../components/ModalContraseña/ModalContraseña';
+
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');  
+  const [isLoading, setIsLoading] = useState(false); // Estado para controlar la pantalla de carga
+
+  // Data del formulario
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  //History, segun sive para hacer el push al Home con el metodo post.
+  const history = useHistory(); // Obtén el objeto history
+ 
+  // Ventana emergente de éxito de acceso
+  const [showSuccessAccesoToast, setShowSuccessAccesoToast] = useState(false);
+  // Ventana emergente de datos no válidos
+  const [showInvalidDataToast, setShowInvalidDataToast] = useState(false);
+  
+ 
+  const handleLogin = async () => {
+    setIsLoading(true); // Mostrar pantalla de carga
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    //Publica el registro en la api
+    try {
+      // Simular una solicitud de inicio de sesión con una espera de 3 segundos
+      await new Promise(resolve => setTimeout(resolve, 2000));
+
+      // Aquí realizarías la solicitud de inicio de sesión real con axios
+       const response = await axios.post('http://127.0.0.1:8000/api/login', formData);
+      
+      setIsLoading(false); // Ocultar pantalla de carga
+      
+      // Manejar la respuesta del servidor, por ejemplo, guardar el token en el almacenamiento local o en cookies
+      console.log('Inicio de sesión exitoso');
+
+      // Redirigir al usuario a la página de inicio después de iniciar sesión
+      history.push('/home');
+    } catch (error) {
+        // Mostrar el Toast de datos no válidos
+        setShowInvalidDataToast(true);
+      console.error('Error en el inicio de sesión:', error);
+    }
+  };
+
+
+  return (
+    <IonPage>
+      <IonContent fullscreen className='LoginPage'>
+        <IonHeader>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12" size-lg="6" class="ion-show-sm ion-align-items-center">
+              <img src="/images/logo.png" alt="Logo" className="logo" />
+            </IonCol>
+            <IonCol size="12" size-lg="6" class="ion-show-sm ion-align-items-center">
+            <div className="form-container">
+                <IonInput
+                  type="email"
+                  placeholder="Correo electrónico"
+                  value={formData.email}
+                  onIonChange={(e) => setEmail(e.detail.value!)}
+                  className="button-margin"
+                />
+                <IonInput
+                  type="password"
+                  placeholder="Contraseña"
+                  value={formData.password}
+                  onIonChange={(e) => setPassword(e.detail.value!)}
+                  className="button-margin"
+                />
+                <IonButton expand='full' fill='solid' onClick={handleLogin} className="button-margin">
+                    Acceso
+                </IonButton>
+          <ModalContraseña></ModalContraseña>
+                <ModalSignUp />
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+      
+      <ToastsComponent
+        showSuccessAccesoToast={showSuccessAccesoToast}
+        showInvalidDataToast={showInvalidDataToast}
+        setShowSuccessAccesoToast={setShowSuccessAccesoToast}
+        setShowInvalidDataToast={setShowInvalidDataToast}
+      />
+  <IonLoading
+        isOpen={isLoading}
+        message={'Cargando...'}
+        duration={2000} // Duración de la pantalla de carga (3 segundos en este caso)
+      />
+    </IonPage>
+  );
+};
+
+export default Login;

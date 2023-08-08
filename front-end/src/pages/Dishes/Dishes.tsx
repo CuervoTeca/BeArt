535cd5e68226875175c6b3cdbadec0eb727ebbcd
Ken Antonio <SantillanG753@outlook.com>
2023-08-08 16:50:44 -0700

    dishes get

--- front-end/src/pages/Dishes/Dishes.tsx
+++ front-end/src/pages/Dishes/Dishes.tsx
@@ -1,6 +1,6 @@
 import { IonCol, IonGrid, IonRow, IonCard, IonCardContent, IonCardHeader, IonContent, IonButton, IonIcon, IonPopover, IonList, IonItem } from '@ionic/react';
 import { ellipsisVerticalOutline, trashOutline, restaurantOutline } from 'ionicons/icons';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactProSidebar from '../../components/ReactProSidebar/ReactProSidebar';
 import DataTable from 'react-data-table-component';
 import ModalUpdateDishes from '../../components/ModalUpdateDishes/ModalUpdateDishes';
@@ -32,77 +32,38 @@ const Dishes: React.FC = () => {
       console.log(dishId)
     }
   };
-  
-  
-  
-  const [data, setData] = useState([
-    {
-      DishId: '1',
-      DishName: "Paella",
-      NutritionFactsId: 10,
-      Calories: 1000000,
-      Fats: 1,
-      Collesterol: 10,
-      Sodium: 10,
-      Fiber: 0,
-      Carbs: 0,
-      Protein: 15
-    },
-    {
-      DishId: 2,
-      DishName: "Paella",
-      NutritionFactsId: 10,
-      Calories: 1000000,
-      Fats: 1,
-      Collesterol: 10,
-      Sodium: 10,
-      Fiber: 0,
-      Carbs: 0,
-      Protein: 15
-    },
-    {
-      DishId: 3,
-      DishName: "Paella",
-      NutritionFactsId: 10,
-      Calories: 1000000,
-      Fats: 1,
-      Collesterol: 10,
-      Sodium: 10,
-      Fiber: 0,
-      Carbs: 0,
-      Protein: 15
-    },
-    {
-      DishId: 4,
-      DishName: "Paella",
-      NutritionFactsId: 10,
-      Calories: 1000000,
-      Fats: 1,
-      Collesterol: 10,
-      Sodium: 10,
-      Fiber: 0,
-      Carbs: 0,
-      Protein: 15
-    },
-    {
-      DishId: 5,
-      DishName: "Paella",
-      NutritionFactsId: 10,
-      Calories: 1000000,
-      Fats: 1,
-      Collesterol: 10,
-      Sodium: 10,
-      Fiber: 0,
-      Carbs: 0,
-      Protein: 15
-    }
-  ]);
 
+  const [dishes, setDishes] = useState([]);
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    // Obtén el token de autenticación (por ejemplo, desde localStorage)
+    const storedToken = localStorage.getItem('accessToken');
+    console.log(storedToken)
+    if (storedToken) {
+      setToken(storedToken);
+    }
+
+    // Realiza la solicitud GET a la API con el token en la cabecera
+    axios.get('http://127.0.0.1:8000/api/listDishes', {
+      headers: {
+        Authorization: `Bearer ${storedToken}`
+      }
+    })
+      .then(response => {
+        setDishes(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos:', error);
+      });
+  }, [token]);
+  
   const columns =[
     {
       id: "topDishes",
       name: 'Id',
-      selector: DishId => DishId.DishId,
+      selector: row => row.DishID,
       sortable: true,
     },
     {
@@ -168,14 +129,14 @@ const Dishes: React.FC = () => {
     },
     {
       cell: row => <>
-            <IonButton fill='clear' id={row.DishId + 'dish'} onClick={() => openUpdateModal(row.DishId)}>
+            <IonButton fill='clear' id={row.DishID + 'dish'} onClick={() => openUpdateModal(row.DishID)}>
               <IonIcon icon={ellipsisVerticalOutline} /></IonButton>
-              <IonPopover trigger={row.DishId + "dish"} triggerAction="click" className='IonPopover'>
+              <IonPopover trigger={row.DishID + "dish"} triggerAction="click" className='IonPopover'>
                 <IonContent className='RowModal'>
                   <ModalUpdateDishes dishId={selectedDishId} />
                   {/* Resto de los elementos en el popover */}
                   {/* Botón para borrar */}
-               <IonButton fill='clear' color="danger" onClick={() => handleDelete(row.DishId)}>
+               <IonButton fill='clear' color="danger" onClick={() => handleDelete(row.DishID)}>
                  <IonIcon icon={trashOutline} /> Borrar</IonButton>
                 </IonContent>
               </IonPopover>
@@ -205,7 +166,7 @@ const Dishes: React.FC = () => {
                   <IonCardContent>
                     <DataTable
                       columns = {columns}
-                      data = {data}
+                      data = {dishes.data}
                       pagination
                     />
                     <ModalAddDishes  dishId={selectedDishId}></ModalAddDishes>
